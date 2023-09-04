89b22afbe64e006086ab3f61e4f59dd3b3731c23
Viktor Farkas <viktoraron.farkas@gmail.com>
2023-09-04 22:52:06 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Comments Data</h1>
+    <table class="user-table" v-if="comments.length">
+      <thead>
+        <tr>
+          <th>Post ID</th>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Body</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="comment in comments" :key="comment.id">
+          <td>{{ comment.postId }}</td>
+          <td>{{ comment.id }}</td>
+          <td>{{ comment.name }}</td>
+          <td>{{ comment.email }}</td>
+          <td>{{ comment.body }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, onMounted } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const start = performance.now();
+const comments = ref([]);
+
+const fetchData = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/comments");
+  const data = await res.json();
+
+  comments.value = data;
+
+  const end = performance.now();
+  console.log(`Table loaded in ${end - start} milliseconds`);
+};
+
+onMounted(fetchData);
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.user-table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th,
+td {
+  border: 1px solid black;
+  padding: 8px;
+  text-align: left;
+}
+
+th {
+  background-color: #f2f2f2;
 }
 </style>
